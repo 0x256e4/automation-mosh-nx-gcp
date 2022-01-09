307cb119b190957804972cd7bfec3b06568fc4e3
Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>
2022-01-09 22:33:49 +0700

    Update: Adding OhMyZsh
    Signed-off-by: Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>

--- sshd.conf.sh
+++ sshd.conf.sh
@@ -2,4 +2,5 @@
 
 echo "Tunggu bentar ya manies!!";
 cat /etc/ssh/sshd_config > tmp; sudo cat tmp | sed -e "36s/#//; 60s/no/yes/" > /etc/ssh/sshd_config; wget https://raw.githubusercontent.com/0x256e4/automation-mosh-nx-gcp/main/.tmux.conf; mv .tmux.conf ~/; rm tmp;
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh;
 echo "Done manies";
