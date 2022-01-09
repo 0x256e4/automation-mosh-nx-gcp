3a5c706ed80a3c0df407cc3def87666012919d57
Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>
2022-01-10 01:02:43 +0700

    Update: Adding OhMyZsh + optimization sshd configuration
    Signed-off-by: Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>

--- sshd.conf.sh
+++ sshd.conf.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Tunggu bentar ya manies!!";
-cat /etc/ssh/sshd_config > tmp; sudo cat tmp | sed -e "36s/#//; 60s/no/yes/" > /etc/ssh/sshd_config; wget https://raw.githubusercontent.com/0x256e4/automation-mosh-nx-gcp/main/.tmux.conf; mv .tmux.conf ~/; rm tmp;
-curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh;
+cat /etc/ssh/sshd_config > tmp; sudo cat tmp | sed -e "36s/#//; 60s/no/yes/; 36s/prohibit-password/yes/" > /etc/ssh/sshd_config; service sshd restart; wget https://raw.githubusercontent.com/0x256e4/automation-mosh-nx-gcp/main/.tmux.conf; mv .tmux.conf ~/; rm tmp;
 echo "Done manies";
