353fbaa46674c97c3ff677a667e2c3389626322a
Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>
2022-01-10 01:31:13 +0700

    Update: Fix OhMyZsh
    Signed-off-by: Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>

--- sshd.conf.sh
+++ sshd.conf.sh
@@ -2,4 +2,5 @@
 
 echo "Tunggu bentar ya manies!!";
 apt install -y tmux; cat /etc/ssh/sshd_config > tmp; sudo cat tmp | sed -e "36s/#//; 60s/no/yes/; 36s/prohibit-password/yes/" > /etc/ssh/sshd_config; service sshd restart; wget https://raw.githubusercontent.com/0x256e4/automation-mosh-nx-gcp/main/.tmux.conf; mv .tmux.conf ~/; rm tmp;
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 echo "Done manies";
