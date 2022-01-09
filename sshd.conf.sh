629b4d478563a115ddb57119c06659d556fe480b
Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>
2022-01-09 22:28:47 +0700

    Update: Adding tmux configuration
    Signed-off-by: Muhammad Willdhan Arya Putra <aryaputrascorpio@gmail.com>

--- sshd.conf.sh
+++ sshd.conf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Tunggu bentar ya manies!!";
-cat /etc/ssh/sshd_config > tmp; sudo cat tmp | sed -e "36s/#//; 60s/no/yes/" > /etc/ssh/sshd_config; 
+cat /etc/ssh/sshd_config > tmp; sudo cat tmp | sed -e "36s/#//; 60s/no/yes/" > /etc/ssh/sshd_config; wget https://raw.githubusercontent.com/0x256e4/automation-mosh-nx-gcp/main/.tmux.conf; mv .tmux.conf ~/; rm tmp;
 echo "Done manies";
